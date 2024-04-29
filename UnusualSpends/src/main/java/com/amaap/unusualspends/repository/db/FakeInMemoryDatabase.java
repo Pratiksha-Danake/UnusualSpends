ce627d28180495e6bb9190771cc76e75e7d28c75
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-04-29 16:02:59 +0530

    added functionality to save credit card details into the database

--- UnusualSpends/src/main/java/com/amaap/unusualspends/repository/db/FakeInMemoryDatabase.java
+++ UnusualSpends/src/main/java/com/amaap/unusualspends/repository/db/FakeInMemoryDatabase.java
@@ -1,5 +1,6 @@
 package com.amaap.unusualspends.repository.db;
 
+import com.amaap.unusualspends.domain.model.entity.CreditCard;
 import com.amaap.unusualspends.domain.model.entity.Customer;
 import com.amaap.unusualspends.repository.db.exception.CustomerAlreadyExistsException;
 import com.amaap.unusualspends.repository.db.impl.InMemoryDatabase;
@@ -9,6 +10,7 @@ import java.util.List;
 
 public class FakeInMemoryDatabase implements InMemoryDatabase {
     List<Customer> customers = new ArrayList<>();
+    List<CreditCard> cards = new ArrayList<>();
 
     @Override
     public Customer addCustomer(Customer customer) throws CustomerAlreadyExistsException {
@@ -16,5 +18,10 @@ public class FakeInMemoryDatabase implements InMemoryDatabase {
             throw new CustomerAlreadyExistsException("Customer Found With Same Details..!");
         customers.add(customer);
         return customer;
+    }
+
+    public CreditCard addCreditCard(CreditCard creditCardToAdd) {
+       cards.add(creditCardToAdd);
+       return creditCardToAdd;
     }
 }
--- UnusualSpends/src/test/java/com/amaap/unusualspends/repository/db/FakeInMemoryDatabaseTest.java
+++ UnusualSpends/src/test/java/com/amaap/unusualspends/repository/db/FakeInMemoryDatabaseTest.java
@@ -1,6 +1,8 @@
 package com.amaap.unusualspends.repository.db;
 
+import com.amaap.unusualspends.domain.model.entity.CreditCard;
 import com.amaap.unusualspends.domain.model.entity.Customer;
+import com.amaap.unusualspends.domain.model.entity.exception.InvalidCreditCardIdException;
 import com.amaap.unusualspends.domain.model.entity.exception.InvalidCustomerException;
 import com.amaap.unusualspends.repository.db.exception.CustomerAlreadyExistsException;
 import org.junit.jupiter.api.Test;
@@ -9,11 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FakeInMemoryDatabaseTest {
+    FakeInMemoryDatabase fakeInMemoryDatabase = new FakeInMemoryDatabase();
+
     @Test
     void shouldBeAbleToAddCustomerIntoCustomerTable() throws InvalidCustomerException, CustomerAlreadyExistsException {
         // arrange
         Customer customerToAdd = Customer.create(1, "Pratiksha Danake", "pratiksha@gmail.com");
-        FakeInMemoryDatabase fakeInMemoryDatabase = new FakeInMemoryDatabase();
 
         // act
         Customer customerAdded = fakeInMemoryDatabase.addCustomer(customerToAdd);
@@ -26,12 +29,22 @@ class FakeInMemoryDatabaseTest {
     void shouldThrowCustomerAlreadyExistsExceptionIfCustomerWithSameDetailsFoundInDatabase() throws InvalidCustomerException, CustomerAlreadyExistsException {
         // arrange
         Customer customerToAdd = Customer.create(1, "Pratiksha Danake", "pratiksha@gmail.com");
-        FakeInMemoryDatabase fakeInMemoryDatabase = new FakeInMemoryDatabase();
 
         // act && assert
         fakeInMemoryDatabase.addCustomer(customerToAdd);
         assertThrows(CustomerAlreadyExistsException.class,()->{
             fakeInMemoryDatabase.addCustomer(customerToAdd);
         });
+    }
+
+    @Test
+    void shouldBeAbleToAddCreditCardIntoDatabase() throws InvalidCustomerException, InvalidCreditCardIdException {
+        // arrange
+        Customer customer = Customer.create(1, "Pratiksha Danake", "pratiksha@gmail.com");
+        long cardId = 1;
+        CreditCard creditCardToAdd = CreditCard.create(cardId,customer);
+
+        // act
+        CreditCard cardAdded = fakeInMemoryDatabase.addCreditCard(creditCardToAdd);
     }
 }
