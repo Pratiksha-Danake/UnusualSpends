bc5809f907116f7b8ef52616022a6e7ee97575c7
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-04-29 13:08:17 +0530

    added test cases for CustomerIdValidator class

---
+++ UnusualSpends/src/test/java/com/amaap/unusualspends/domain/model/entity/validator/CustomerIdValidatorTest.java
@@ -0,0 +1,27 @@
+package com.amaap.unusualspends.domain.model.entity.validator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerIdValidatorTest {
+    @Test
+    void shouldBeAbleToReturnTrueIfCustomerIdIsValid(){
+        // arrange
+        int customerId = 1;
+
+        // act && assert
+        Assertions.assertTrue(CustomerIdValidator.isValidId(customerId));
+    }
+
+    @Test
+    void shouldBeAbleToReturnFalseIfCustomerIdIsInvalid(){
+        // arrange
+        int customerId = -1;
+
+        // act && assert
+        Assertions.assertFalse(CustomerIdValidator.isValidId(customerId));
+        Assertions.assertFalse(CustomerIdValidator.isValidId(0));
+    }
+}
